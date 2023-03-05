28732dc787da421eca355963c2129493a62ab04f
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-05 19:59:03 +0900

    【FE_1周目】認証ページの実装 #8

--- 230213_learning01_frontend/components/Layout.tsx
+++ 230213_learning01_frontend/components/Layout.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head';
 import { FC, ReactNode } from 'react';
+import Head from 'next/head';
 
 type Props = {
   title: string;
@@ -10,10 +10,10 @@ export const Layout: FC<Props> = ({ children, title = 'Nextjs' }) => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <Head>
-        <title>{ title }</title>
+        <title>{title}</title>
       </Head>
-      <main className='flex w-screen flex-1 flex-col items-center justify-center'>
-        { children }
+      <main className="flex w-screen flex-1 flex-col items-center justify-center">
+        {children}
       </main>
     </div>
   );
