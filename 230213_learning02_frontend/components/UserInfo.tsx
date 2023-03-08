978e6093e39b6376d75f2318c2a9e39cedcec61a
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-08 09:35:54 +0900

    【FE_2周目】UserInfo（Components）の実装 #10

---
+++ 230213_learning02_frontend/components/UserInfo.tsx
@@ -0,0 +1,9 @@
+import { Loader } from '@mantine/core'
+import React from 'react'
+import { useQueryUser } from '../hooks/useQueryUser'
+
+export const UserInfo = () => {
+  const { data: user, status } = useQueryUser()
+  if (status === 'loading') return <Loader />
+  return <p>{user?.email}</p>
+}
