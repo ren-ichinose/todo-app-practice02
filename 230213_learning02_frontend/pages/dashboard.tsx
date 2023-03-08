e227b18d0b5cdb6b501598c8cee5e03367083a37
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-08 09:37:31 +0900

    【FE_2周目】ユーザー情報の表示とログアウト時のキャシュの削除機能の追加 #11

--- 230213_learning02_frontend/pages/dashboard.tsx
+++ 230213_learning02_frontend/pages/dashboard.tsx
@@ -1,22 +1,26 @@
 import { LogoutIcon } from '@heroicons/react/solid'
+import { useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { Layout } from '../components/Layout'
+import { UserInfo } from '../components/userInfo'
 
 const Dashboard: NextPage = () => {
   const router = useRouter()
+  const queryClient = useQueryClient()
   const logout = async () => {
+    queryClient.removeQueries(['user'])
     await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`)
     router.push('/')
   }
   return (
     <Layout title="Task borad">
-      {' '}
       <LogoutIcon
         className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={logout}
       />
+      <UserInfo />
     </Layout>
   )
 }
