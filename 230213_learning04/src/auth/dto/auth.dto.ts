9c436579d35b916b3743b4a04a566ecb336345a9
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-11 23:39:12 +0900

    【4周目】Dto&interface(Auth) #3

---
+++ 230213_learning04/src/auth/dto/auth.dto.ts
@@ -0,0 +1,18 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(32)
+  password: string;
+}
