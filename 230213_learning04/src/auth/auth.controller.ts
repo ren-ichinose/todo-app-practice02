de92f1f2593c6f17ac57eee6d2ad07e317095b54
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-12 00:35:49 +0900

    【4周目】SignUp機能の実装(Auth) #3

--- 230213_learning04/src/auth/auth.controller.ts
+++ 230213_learning04/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
+import { Msg } from './interfaces/auth.interface';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signUp(@Body() authDto: AuthDto): Promise<Msg> {
+    return await this.authService.signUp(authDto);
+  }
+}
--- 230213_learning04/src/auth/auth.service.ts
+++ 230213_learning04/src/auth/auth.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcrypt';
+import { Msg } from './interfaces/auth.interface';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async signUp(authDto: AuthDto): Promise<Msg> {
+    const { email, password } = authDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    try {
+      await this.prismaService.user.create({
+        data: {
+          email,
+          hashedPassword,
+        },
+      });
+      return { message: 'OK' };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('This email is already taken');
+        }
+      }
+      throw error;
+    }
+  }
+}
