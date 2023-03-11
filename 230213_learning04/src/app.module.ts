79bd814306964785d626758a6576c4f278b42cdb
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-11 19:25:34 +0900

    【4周目】開発準備 #1

--- 230213_learning04/src/app.module.ts
+++ 230213_learning04/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, TodoModule, PrismaModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
