f7b19b262808608b59b93bb54156e8975f00e7f4
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-13 23:49:32 +0900

    【4周目】ユーザー情報を取得するメソッドの実装 #4

--- 230213_learning04/src/user/user.controller.ts
+++ 230213_learning04/src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator/get-user.decorator';
+import { PrismaService } from 'src/prisma/prisma.service';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  @Get()
+  getLoginUser(
+    @GetUser() user: Omit<User, 'hashedPassword'>,
+  ): Omit<User, 'hashedPassword'> {
+    return user;
+  }
+}
