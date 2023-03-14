e02212e8ab162a8e1a5c1b56e4c98eb27cb7cafe
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-14 23:42:28 +0900

    【4周目】ユーザー情報の更新機能の実装 #4

--- 230213_learning04/src/user/user.controller.ts
+++ 230213_learning04/src/user/user.controller.ts
@@ -1,18 +1,27 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator/get-user.decorator';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from './user.service';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('user')
 export class UserController {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly userService: UserService) {}
 
   @Get()
   getLoginUser(
     @GetUser() user: Omit<User, 'hashedPassword'>,
   ): Omit<User, 'hashedPassword'> {
     return user;
+  }
+
+  @Patch()
+  async updateUser(
+    @Body() updateUserDto: UpdateUserDto,
+    @GetUser() user: Omit<User, 'hashedPassword'>,
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    return await this.userService.updateUser(updateUserDto, user.id)
   }
 }
--- 230213_learning04/src/user/user.service.ts
+++ 230213_learning04/src/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async updateUser(updateUserDto: UpdateUserDto, id: string):Promise<Omit<User, 'hashedPassword'>> {
+    const { nickName } = updateUserDto;
+    const user = await this.prismaService.user.update({
+      where: { id },
+      data: { nickName },
+    });
+    const { hashedPassword, ...userWithoutHashedPassword } = user;
+    return userWithoutHashedPassword;
+  }
+}
