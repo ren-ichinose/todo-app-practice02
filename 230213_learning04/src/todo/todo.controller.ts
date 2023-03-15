16fc19f5b6e3430918af51c59f7b17f692150f3f
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-15 15:51:46 +0900

    【4周目】タスク取得機能の実装 #5

--- 230213_learning04/src/todo/todo.controller.ts
+++ 230213_learning04/src/todo/todo.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Task, User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator/get-user.decorator';
+import { TodoService } from './todo.service';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  async getTasks(@GetUser() user: User): Promise<Task[]> {
+    return await this.todoService.getTasks(user.id);
+  }
+}
--- 230213_learning04/src/todo/todo.service.ts
+++ 230213_learning04/src/todo/todo.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getTasks(userId: string): Promise<Task[]> {
+    const tasks = await this.prismaService.task.findMany({
+      where: { userId },
+      orderBy: { createdAt: 'desc' },
+    });
+    return tasks;
+  }
+}
